3dbcfda44ac9afd5373f0016a02e3e98e4daf26c
zq <2419646091@qq.com>
2025-11-12 22:06:25 +0800

    init

---
+++ src/main/java/hhsc/kangnasi/xyz/jenkinstest/JenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package hhsc.kangnasi.xyz.jenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestApplication.class, args);
+    }
+
+}
